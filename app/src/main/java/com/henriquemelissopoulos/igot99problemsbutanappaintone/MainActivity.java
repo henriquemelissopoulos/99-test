d8efd84526e14e917ef1995e6c533fa1fea9a324
Henrique <henriquemelissopoulos@gmail.com>
2015-11-30 19:27:46 -0200

    Move to AppCompatActivity and enable user GPS location.

--- app/src/main/java/com/henriquemelissopoulos/igot99problemsbutanappaintone/MainActivity.java
+++ app/src/main/java/com/henriquemelissopoulos/igot99problemsbutanappaintone/MainActivity.java
@@ -1,7 +1,7 @@
 package com.henriquemelissopoulos.igot99problemsbutanappaintone;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap googleMap;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +23,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-
+        googleMap.setMyLocationEnabled(true);
         LatLng sydney = new LatLng(-34, 151);
-        this.googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
