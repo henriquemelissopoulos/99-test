1c7f900961d5dde384cd841ddd68a0b106f0eb3c
Henrique <henriquemelissopoulos@gmail.com>
2015-12-01 02:10:58 -0200

    Open map at user location and request permission if needed.

--- app/src/main/java/com/henriquemelissopoulos/igot99problemsbutanappaintone/MainActivity.java
+++ app/src/main/java/com/henriquemelissopoulos/igot99problemsbutanappaintone/MainActivity.java
@@ -1,34 +1,92 @@
 package com.henriquemelissopoulos.igot99problemsbutanappaintone;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 1;
+
+    GoogleMap map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (savedInstanceState == null) {
+            mapFragment.setRetainInstance(true);
+        }
+
         mapFragment.getMapAsync(this);
     }
 
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.setMyLocationEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.getUiSettings().setCompassEnabled(false);
+    public void onMapReady(GoogleMap map) {
+        this.map = map;
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setCompassEnabled(false);
 
-        LatLng sydney = new LatLng(-34, 151);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        zoomUser();
     }
+
+
+    public void zoomUser() {
+        Location userLocation;
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            String provider = locationManager.getBestProvider(new Criteria(), true);
+            userLocation = locationManager.getLastKnownLocation(provider);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
+            return;
+        }
+
+        if (map != null && userLocation != null) {
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(userLocation.getLatitude(), userLocation.getLongitude()))
+                    .zoom(15)
+                    .build();
+
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSION_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                zoomUser();
+            } else {
+                Toast.makeText(this, R.string.general_permission_ask, Toast.LENGTH_SHORT).show();
+                zoomUser();
+            }
+        }
+    }
+
 }
